576a3c177257d5a623768ae5651c41c34720a193
Maciej Lipiński <maciej@statlook.com>
2024-02-19 15:18:50 +0100

    data service - loading data refactoring

--- src/app/areas/list/list-data.service.ts
+++ src/app/areas/list/list-data.service.ts
@@ -9,14 +9,29 @@ import { firstValueFrom } from 'rxjs';
 })
 export class ListDataService {
   private apiUrl = 'assets/comix-data.json';
+  private comixItems: IComixItem[] = [];
+  private savedComixItemsLoaded: boolean = false;
 
   constructor(private http: HttpClient) { }
 
   async getComixListData(): Promise<IComixItem[]> {
-    return firstValueFrom(this.http.get<IComixItem[]>(this.apiUrl));
+    if (!this.comixItems.length || !this.savedComixItemsLoaded) {
+      try {
+        const data = await firstValueFrom(this.http.get<IComixItem[]>(this.apiUrl));
+        this.comixItems = data;
+        this.savedComixItemsLoaded = true;
+        return data;
+      } catch (error) {
+        this.savedComixItemsLoaded = false;
+        console.error('Failed to load comix data', error);
+        throw error;
+      }
+    }
+    return this.comixItems;
   }
 
   async saveComixListData(comixData: IComixItem[]): Promise<any> {
+    this.savedComixItemsLoaded = false;
     return firstValueFrom(this.http.post(this.apiUrl, comixData));
   }
 }
